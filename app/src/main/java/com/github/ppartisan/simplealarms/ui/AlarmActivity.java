e338f292d55c1d7386d5da7ee9ba5ed0bc4237ce
SaluKhadka95 <salukhadka95@gmail.com>
2018-04-21 17:32:30 +0545

    initial commit

---
+++ app/src/main/java/com/github/ppartisan/simplealarms/ui/AlarmActivity.java
@@ -0,0 +1,18 @@
+package com.github.ppartisan.simplealarms.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.ppartisan.simplealarms.R;
+
+/**
+ * Created by acer on 4/21/2018.
+ */
+
+public class AlarmActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/github/ppartisan/simplealarms/ui/MainActivity.java
@@ -0,0 +1,30 @@
+package com.github.ppartisan.simplealarms.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.ppartisan.simplealarms.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Button btnAlarm = (Button) findViewById(R.id.btnAlarm);
+        btnAlarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+    }
+
+}
