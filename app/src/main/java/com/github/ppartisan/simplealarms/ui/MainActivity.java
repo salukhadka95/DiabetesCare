f197a5b0e2cf0ef609087bf646c980c1472e16cf
Anushka <anushkasubedi3174@gmail.com>
2018-05-03 08:42:03 +0545

    layout made better

--- app/src/main/java/com/github/ppartisan/simplealarms/ui/MainActivity.java
+++ app/src/main/java/com/github/ppartisan/simplealarms/ui/MainActivity.java
@@ -1,10 +1,13 @@
 package com.github.ppartisan.simplealarms.ui;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.github.ppartisan.simplealarms.R;
 
@@ -14,7 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Button btnAlarm = (Button) findViewById(R.id.btnAlarm);
+        ImageButton btnAlarm = (ImageButton) findViewById(R.id.btnAlarm);
+        ImageView menu=(ImageView) findViewById(R.id.menu);
+
         btnAlarm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,6 +28,14 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
